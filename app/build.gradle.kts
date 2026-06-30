6273754f2ee62fb351bbd382b8f8c8d5e6eb6d65
vllasa723-ctrl <vllasa723@gmail.com>
2026-06-30 16:33:51 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.speedometer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.speedometer"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+}
